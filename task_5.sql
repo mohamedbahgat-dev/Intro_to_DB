0d3c25fce270c5c46a22fb6c81c8bd93fb6ac758
mohamedbahgat <mohamedbahgat950@gmail.com>
2025-08-16 07:03:01 +0300

    insert data into a table

--- task_5.sql
+++ task_5.sql
@@ -1,2 +1,5 @@
 INSERT INTO Customers (customer_id, customer_name, email, address)
 Values (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.' );
+
+INSERT INTO customer (customer_id, customer_name, email, address)
+Values (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.' );
